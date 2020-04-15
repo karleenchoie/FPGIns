39196bf0c4e5ee358d371596ed58f850a2489303
karleenchoie <43874922+karleenchoie@users.noreply.github.com>
2020-04-15 08:54:37 +0800

    -Branch locator

---
+++ app/src/main/java/com/example/fpgins/DataModel/BranchData.java
@@ -0,0 +1,77 @@
+package com.example.fpgins.DataModel;
+
+public class BranchData {
+    private String id;
+    private String address;
+    private String contact_no;
+    private String email;
+    private String fax_no;
+    private String office_country_name;
+    private String office_type_name;
+
+    public BranchData(String id, String address, String contact_no, String email, String fax_no, String office_country_name, String office_type_name) {
+        this.id = id;
+        this.address = address;
+        this.contact_no = contact_no;
+        this.email = email;
+        this.fax_no = fax_no;
+        this.office_country_name = office_country_name;
+        this.office_type_name = office_type_name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getContact_no() {
+        return contact_no;
+    }
+
+    public void setContact_no(String contact_no) {
+        this.contact_no = contact_no;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFax_no() {
+        return fax_no;
+    }
+
+    public void setFax_no(String fax_no) {
+        this.fax_no = fax_no;
+    }
+
+    public String getOffice_country_name() {
+        return office_country_name;
+    }
+
+    public void setOffice_country_name(String office_country_name) {
+        this.office_country_name = office_country_name;
+    }
+
+    public String getOffice_type_name() {
+        return office_type_name;
+    }
+
+    public void setOffice_type_name(String office_type_name) {
+        this.office_type_name = office_type_name;
+    }
+}
--- app/src/main/java/com/example/fpgins/ui/BranchLocator/BranchLocator.java
+++ app/src/main/java/com/example/fpgins/ui/BranchLocator/BranchLocator.java
@@ -1,33 +1,50 @@
 package com.example.fpgins.ui.BranchLocator;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
-import android.widget.ScrollView;
-import android.widget.TextView;
+import android.widget.Toast;
 
+import com.example.fpgins.DataModel.BranchData;
+import com.example.fpgins.Network.Cloud;
 import com.example.fpgins.R;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
 
 public class BranchLocator extends AppCompatActivity {
 
     private ImageView mBackButton;
-    private TextView mPhilippines;
-    private LinearLayout mPhBranch;
-    private boolean isDisplayPh = false;
-    private ScrollView mScrollView;
+    private ArrayList<BranchData> mData = new ArrayList<>();
+    private BranchAdapter mAdapter;
+    private RecyclerView mRecyclerView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_branch_locator);
 
-        mPhilippines = findViewById(R.id.txt_Ph);
-        mPhBranch = findViewById(R.id.linear_phBranch);
-        mScrollView = findViewById(R.id.sv_Branch);
+        mRecyclerView = findViewById(R.id.mainOffice_recyclerview);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(
+                getApplicationContext(), LinearLayoutManager.VERTICAL, false);
+        mRecyclerView.setLayoutManager(layoutManager);
+        mRecyclerView.setItemAnimator(new DefaultItemAnimator());
 
+//        mRecyclerView.setLayoutManager(new LinearLayoutManager(BranchLocator.this));
+//        mRecyclerView.setItemAnimator(new DefaultItemAnimator());
+
+        getBranch();
+
+        mAdapter = new BranchAdapter(mData, BranchLocator.this);
         mBackButton = findViewById(R.id.img_backbutton);
         mBackButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -36,19 +53,67 @@ public class BranchLocator extends AppCompatActivity {
             }
         });
 
-        mPhilippines.setOnClickListener(new View.OnClickListener() {
+    }
+
+    public void getBranch(){
+        mData.clear();
+        Cloud.getOfficeAddress(new Cloud.ResultListener() {
             @Override
-            public void onClick(View v) {
-                isDisplayPh = !isDisplayPh;
-                if (isDisplayPh == true){
-                    mPhBranch.setVisibility(View.VISIBLE);
-                    mPhilippines.setCompoundDrawablesWithIntrinsicBounds(0, 0, R.drawable.arrow_up, 0);
-                }else {
-                    mPhBranch.setVisibility(View.GONE);
-                    mPhilippines.setCompoundDrawablesWithIntrinsicBounds(0, 0, R.drawable.arrow_down, 0);
+            public void onResult(JSONObject result) {
+                int returnCode;
+                JSONObject jsonObject = new JSONObject();
+
+                try {
+                    jsonObject = result;
+                    returnCode = Integer.parseInt(jsonObject.get("code").toString());
+                } catch (JSONException e){
+                    returnCode = Cloud.DefaultReturnCode.INTERNAL_SERVER_ERROR;
+                    e.printStackTrace();
+                }
+
+                if (returnCode != Cloud.DefaultReturnCode.SUCCESS){
+                    //FAIL
+                    try {
+                        String message = jsonObject.getString("message");
+                        Toast.makeText(BranchLocator.this, message, Toast.LENGTH_SHORT).show();
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                } else {
+                    //SUCCESS
+                    try {
+                        JSONArray jsonArray = jsonObject.getJSONArray("record");
+                        generateResult(jsonArray);
+                        mRecyclerView.setAdapter(mAdapter);
+                    } catch (Exception e) {
+                        e.getMessage();
+                    }
                 }
             }
         });
+
+    }
+
+    private void generateResult(JSONArray jsonArray){
+        try {
+            for (int i = 0; i < jsonArray.length(); i++){
+                JSONObject jsonObject = jsonArray.getJSONObject(i);
+                String id = jsonObject.getString("id");
+                String address = jsonObject.getString("address");
+                String contact_no = jsonObject.getString("contact_no");
+                String email = jsonObject.getString("email");
+                String fax_no = jsonObject.getString("fax_no");
+                String office_country_name = jsonObject.getString("office_country_name");
+                String office_type_name = jsonObject.getString("office_type_name");
+
+                BranchData data = new BranchData(id, address, contact_no, email, fax_no, office_country_name, office_type_name);
+                mData.add(data);
+            }
+
+
+        } catch (Exception e){
+            e.printStackTrace();
+        }
     }
 
     @Override
